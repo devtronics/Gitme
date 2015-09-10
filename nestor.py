2543b671dd32c4ba6f760691c91ed6cc6a7cd0ce
chrisheckler <hecklerchris@hotmail.com>
2015-09-10 17:26:11 -0600

    Comments added via VIM

--- nestor.py
+++ nestor.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 # authored by chris heckler
 
+
 def main():
 	print("This is my first module")
+
+""" This function takes a positional argument called 'the_list' which is any Pytohon list(of, possibly, nested lists) Each data item in the provided list is (recursively) printed to the screen on its own line. Vim is becoming fun to work with as well. """
 
 def print_lol(the_list):
 	for each_item in the_list:
