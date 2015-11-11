62288e929ceb93ada44f11b5d28a028e863a092e
chrisheckler <hecklerchris@hotmail.com>
2015-11-10 22:05:43 -0800

    update to code

--- supermarket_checkout.py
+++ supermarket_checkout.py
@@ -1,3 +1,17 @@
 shopping_list = ["banana", "orange", "pear", "apple"]
 
+stock = {
+	"banana": 6,
+	"apple": 0,
+	"orange": 32,
+	"pear": 15,
+}
+
+prices = {
+	"banana": 4,
+	"apple": 2,
+	"orange": 1.5,
+	"pear": 3,
+}
+
 
