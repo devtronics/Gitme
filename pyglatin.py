90c9a7e99399689281eaf20505e805818c6fcc5e
chrisheckler <hecklerchris@hotmail.com>
2015-11-16 08:14:45 -0800

    finshed pyglatin program

--- pyglatin.py
+++ pyglatin.py
@@ -5,4 +5,14 @@ original = raw_input("Enter a word:")
 if len(original) > 0 and orginal.isalpha():
 	word = original.lower()
 	first = original[0]
+	
+	if first == "a" or first == "e" or first == "i" or 
+	first == "u":
+		new_word = word[1:len(word)] + first + pyg
+		print new_word
+	else:
+		print "consonant"
 
+else:
+	print "empty"
+ 
