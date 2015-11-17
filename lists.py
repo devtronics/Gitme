b1f14ff57a08d1023323da25cf44ba461e5329b8
James Beedy <jbeedy@jbeedy-laptop.local>
2015-11-16 21:42:35 -0800

    Added comment to indicate importance of 4 space indentation in python.

--- lists.py
+++ lists.py
@@ -1,10 +1,12 @@
 n = [[1, 2, 3], [4,5,6,7,8,9]]
 
+# Always ensure to indent by 4 spaces
+
 def flatten(lists):
-	results = []
-	for numbers in lists:
-		for item in numbers:
-			results.append(item)
-	return results
+    results = []
+    for numbers in lists:
+        for item in numbers:
+            results.append(item)
+    return results
 
 print flatten(n)
