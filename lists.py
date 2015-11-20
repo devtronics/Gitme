3154562fabee02798ed42c52c56b0d926b2385af
chrisheckler <hecklerchris@hotmail.com>
2015-11-19 19:23:32 -0800

    lists

--- lists.py
+++ lists.py
@@ -1,10 +1,12 @@
-n = [[1, 2, 3], [4,5,6,7,8,9]]
+n= [[1, 2, 3], [4,5,6,7,8,9]]
 
-def flatten(lists):
-	results = []
+def flatten(lists)
+    results = []
 	for numbers in lists:
 		for item in numbers:
 			results.append(item)
 	return results
 
 print flatten(n)
+
+
