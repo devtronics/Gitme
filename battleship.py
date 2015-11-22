ec67b42f895b353e5c48a96b45ca880ee6f5f937
chrisheckler <hecklerchris@hotmail.com>
2015-11-22 13:19:38 -0800

    Finished Battleship game

--- battleship.py
+++ battleship.py
@@ -1,15 +1,10 @@
 from random import randint
 
 board = []
-guess_row = int(raw_input("Guess Row: "))
-guess_col = int(raw_input("Guess Col: "))
-# takes userinput for row and col to "seek" hidden ship
 
 
-
- for number in range(5):
+for number in range(5):
     o = ["O"] * 5
-    print o
     board.append(o)
     # created 5 rows with 5 Os to
     #create the battleship board """
@@ -30,22 +25,28 @@ def random_col(board):
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-print ship_row
-print ship_col
 
-for turn in range(4);
+
+for turn in range(4):
     print "Turn", turn + 1
-     if guess_row == ship_row and guess_col == ship_col:
-        print "Congratulations! You sank my battleship!"
 
+    guess_row = int(raw_input("Guess Row: "))
+    guess_col = int(raw_input("Guess Col: "))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sank my battleship!"
+        break
     else:
 
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
             print "Oops, that's not even in the ocean."
         elif board[guess_row][guess_col] == "X":
             print "You guessed that one already."
-         else:
+        else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "X"
 
+    if turn == 3:
+        print "Game Over"
+        break
     print_board(board)
