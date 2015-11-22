83dee773d6d57a18f218c73d0bef9c3e5b488cbc
chrisheckler <hecklerchris@hotmail.com>
2015-11-22 12:29:39 -0800

    turn iteration added, code cleaned up

--- battleship.py
+++ battleship.py
@@ -5,7 +5,9 @@ guess_row = int(raw_input("Guess Row: "))
 guess_col = int(raw_input("Guess Col: "))
 # takes userinput for row and col to "seek" hidden ship
 
-for number in range(5):
+
+
+ for number in range(5):
     o = ["O"] * 5
     print o
     board.append(o)
@@ -16,6 +18,7 @@ def print_board(board):
     for row in board:
         print " ".join(row)
     # comma's and brackets removed and board cleaned up
+print "Let's play Battleship!"
 
 def random_row(board):
     return randint(0, len(board) - 1)
@@ -30,18 +33,19 @@ ship_col = random_col(board)
 print ship_row
 print ship_col
 
-if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sank my battleship!"
-
-else:
-    print "You missed my battleship!"
-    board[guess_row][guess_col] = "X"
-    print print_board(board)
-    # if guess is wrong returns message and an X on wrong guesss
-
-    if guess_row not in range(5) or guess_col not in range(50)
-        print "Oops, that's not even in the ocean."
+for turn in range(4);
+    print "Turn", turn + 1
+     if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sank my battleship!"
 
     else:
-        print "You missed my battleship!"
 
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+        elif board[guess_row][guess_col] == "X":
+            print "You guessed that one already."
+         else:
+            print "You missed my battleship!"
+            board[guess_row][guess_col] = "X"
+
+    print_board(board)
