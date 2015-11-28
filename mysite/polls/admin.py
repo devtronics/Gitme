e6da35165e4eb9affa6af9200689072cc4052e15
chrisheckler <hecklerchris@hotmail.com>
2015-11-28 15:14:56 -0800

    addition to admin.py, enabled choices to be selected

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
 
 
@@ -10,6 +10,7 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
 
 
 
